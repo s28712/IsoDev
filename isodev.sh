27ab87f44b9d993ad13ca66a75dcce40aa621fa2
simar-singh <simar.singh.r@gmail.com>
2021-12-05 19:49:11 -0500

    Added isosync command
    Added the isosync command to sync the main content with the shared content

--- isodev.sh
+++ isodev.sh
@@ -19,9 +19,9 @@ help1 () {
 run() {
   # Check if help prompt
   if [[ $1 == "help" ]] || [[ $1 == "-h" ]]; then 
-    echo -e -e "Run options:"
-    echo -e -e "   *             Destroy and run isolated enviornment"
-    echo -e -e "   reload [-l]   Reload isolated enviornment"
+    echo -e "Run options:"
+    echo -e "   *             Destroy and run isolated enviornment"
+    echo -e "   reload [-l]   Reload isolated enviornment"
   # Reload vm
   elif [[ $1 == "reload" ]] || [[ $1 == "-l" ]]; then
     echo -e "reload"
@@ -33,57 +33,61 @@ run() {
   fi
 
   # Print ssh config
-  echo -e -e "\n"
+  echo -e "\n"
   vagrant ssh-config
-  echo -e -e "\n Copy and paste the above section into your ssh config to add vscode ssh capabilities"
+  echo -e "\n Copy and paste the above section into your ssh config to add vscode ssh capabilities"
 }
 
 # Generate command
 generate() {
   # Check help prompt
   if [[ $1 == "help" ]] || [[ $1 == "-h" ]]; then 
-    echo -e -e "\nGenerate help\n"
+    echo -e "\nGenerate help\n"
   
-    echo -e -e "isodev generate [options] \n"
+    echo -e "isodev generate [options] \n"
 
-    echo -e -e "Available Generations:"
-    echo -e -e "   node [-n]     NodeJS"
-    echo -e -e "   python [-p]   Python3"
-    echo -e -e "   go [-g]       Golang"
+    echo -e "Available Generations:"
+    echo -e "   node [-n]     NodeJS"
+    echo -e "   python [-p]   Python3"
+    echo -e "   go [-g]       Golang"
   # Generate Vagrantfile
   else 
     : > $PWD/Vagrantfile
-    echo -e -e "# -*- mode: ruby -*-" >> $PWD/Vagrantfile
-    echo -e -e "# vi: set ft=ruby :\n" >> $PWD/Vagrantfile
-    echo -e -e "\$script = <<-SCRIPT" >> $PWD/Vagrantfile
+    echo -e "# -*- mode: ruby -*-" >> $PWD/Vagrantfile
+    echo -e "# vi: set ft=ruby :\n" >> $PWD/Vagrantfile
+    echo -e "\$script = <<-SCRIPT" >> $PWD/Vagrantfile
 
     # Check for language configuration
     if [[ $1 == "node" ]] || [[ $1 == "-n" ]]; then
-      echo -e -e "curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -" >> $PWD/Vagrantfile
-      echo -e -e "sudo apt-get install -y nodejs" >> $PWD/Vagrantfile
+      echo -e "curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -" >> $PWD/Vagrantfile
+      echo -e "sudo apt-get install -y nodejs" >> $PWD/Vagrantfile
     elif [[ $1 == "python" ]] || [[ $1 == "-p" ]]; then
-      echo -e -e "sudo apt install build-essential -y" >> $PWD/Vagrantfile
-      echo -e -e "sudo apt install python3 python3-dev -y" >> $PWD/Vagrantfile
+      echo -e "sudo apt install build-essential -y" >> $PWD/Vagrantfile
+      echo -e "sudo apt install python3 python3-dev -y" >> $PWD/Vagrantfile
     else 
       true
     fi
 
     # Continue Vagrantfile generation
-    echo -e -e "mkdir /home/vagrant/isodev" >> $PWD/Vagrantfile
-    echo -e -e "cp -fr -a /vagrant/. /home/vagrant/isodev/" >> $PWD/Vagrantfile
-    echo -e -e "SCRIPT\n" >> $PWD/Vagrantfile
+    echo -e "mkdir /home/vagrant/isodev" >> $PWD/Vagrantfile
+    echo -e "cp -fr -a /vagrant/. /home/vagrant/isodev/" >> $PWD/Vagrantfile
+    echo "echo -e \"rsync -av --progress /home/vagrant/isodev/. /vagrant --exclude node_modules --exclude .vagrant --exclude .git\" > isosync" >> $PWD/Vagrantfile
+    echo -e "chmod +x isosync" >> $PWD/Vagrantfile
+    echo -e "sudo chmod +x isosync" >> $PWD/Vagrantfile
+    echo -e "sudo mv isosync /usr/local/bin" >> $PWD/Vagrantfile
+    echo -e "SCRIPT\n" >> $PWD/Vagrantfile
 
-    echo -e -e "Vagrant.configure("2") do |config|" >> $PWD/Vagrantfile
-    echo -e -e "\tconfig.vm.provider \"virtualbox\" do |v|" >> $PWD/Vagrantfile
-    echo -e -e "\t\tv.memory = 512" >> $PWD/Vagrantfile
-    echo -e -e "\t\tv.cpus = 1" >> $PWD/Vagrantfile
-    echo -e -e "\t\tv.name = \"isodev\"" >> $PWD/Vagrantfile
-    echo -e -e "\tend\n" >> $PWD/Vagrantfile
+    echo -e "Vagrant.configure("2") do |config|" >> $PWD/Vagrantfile
+    echo -e "\tconfig.vm.provider \"virtualbox\" do |v|" >> $PWD/Vagrantfile
+    echo -e "\t\tv.memory = 512" >> $PWD/Vagrantfile
+    echo -e "\t\tv.cpus = 1" >> $PWD/Vagrantfile
+    echo -e "\t\tv.name = \"isodev\"" >> $PWD/Vagrantfile
+    echo -e "\tend\n" >> $PWD/Vagrantfile
 
-    echo -e -e "\tconfig.vm.box = \"hashicorp/bionic64\"" >> $PWD/Vagrantfile
-    echo -e -e "\tconfig.vm.synced_folder \".\", \"/vagrant\", type: \"virtualbox\"" >> $PWD/Vagrantfile
-    echo -e -e "\tconfig.vm.provision \"shell\", inline: \$script" >> $PWD/Vagrantfile
-    echo -e -e "end" >> $PWD/Vagrantfile
+    echo -e "\tconfig.vm.box = \"hashicorp/bionic64\"" >> $PWD/Vagrantfile
+    echo -e "\tconfig.vm.synced_folder \".\", \"/vagrant\", type: \"virtualbox\"" >> $PWD/Vagrantfile
+    echo -e "\tconfig.vm.provision \"shell\", inline: \$script" >> $PWD/Vagrantfile
+    echo -e "end" >> $PWD/Vagrantfile
   fi
 }
 
